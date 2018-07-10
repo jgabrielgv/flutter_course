c2078ab4af77cbd342a01b4257323c2ff0e71e41
Juan Gabriel Gomez Vargas <jgmezvargas@gmail.com>
2018-07-09 22:12:07 -0600

    adding the first stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 
 main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food tester'];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -11,11 +21,32 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('EasyList'),
         ),
-        body: Card(child: Column(children: <Widget>[
-          Image.asset('assets/food.jpg'),
-          Text('Food Paradise')
-            ],
-          ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    _products.add('Advanced Food Tester');
+                  });
+                },
+                child: Text('Add Product'),
+              ),
+            ),
+            Column(
+              children: _products
+                  .map((element) => Card(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset('assets/food.jpg'),
+                            Text(element)
+                          ],
+                        ),
+                      ))
+                  .toList(),
+            )
+          ],
         ),
       ),
     );
