276ec642f35e1530e9cb80ad5314e0fe24375b12
= <jgmezvargas@gmail.com>
2018-07-11 16:00:56 -0600

    splitting components

---
+++ lib/product_manager.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import './products.dart';
+
+class ProductManager extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ProductManagerState();
+  }
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products = ['Food tester'];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(children: <Widget>[
+      Container(
+        margin: EdgeInsets.all(10.0),
+        child: RaisedButton(
+          onPressed: () {
+            setState(() {
+              _products.add('Advanced Food Tester');
+            });
+          },
+          child: Text('Add Product'),
+        ),
+      ),
+      Products(_products)
+    ]);
+  }
+}
