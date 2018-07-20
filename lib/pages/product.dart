f4208b51d4e719da98eebd4fbd4656d4e129b412
= <jgmezvargas@gmail.com>
2018-07-20 15:55:55 -0600

    adding auth page

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,7 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
-
   final String title;
   final String imageUrl;
 
@@ -10,27 +11,33 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.asset(imageUrl),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: Text(title),
+    return WillPopScope(
+        onWillPop: () {
+          print('Back button pressed');
+          Navigator.pop(context, false);
+          return Future.value(false);
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(title),
           ),
-          Container(
-              padding: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                child: Text('DELETE'),
-                onPressed: () => Navigator.pop(context, true),
-              ))
-        ],
-      ),
-    );
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(imageUrl),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text(title),
+              ),
+              Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Theme.of(context).accentColor,
+                    child: Text('DELETE'),
+                    onPressed: () => Navigator.pop(context, true),
+                  ))
+            ],
+          ),
+        ));
   }
 }
